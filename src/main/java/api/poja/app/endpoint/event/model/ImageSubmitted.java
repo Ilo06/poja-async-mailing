96fc7f003a9f5496901781445549e35f45879a83
ilo06 <hei.iloniaina@gmail.com>
2026-07-13 08:47:24 +0300

    feat: image submitted event class

---
+++ src/main/java/api/poja/app/endpoint/event/model/ImageSubmitted.java
@@ -0,0 +1,33 @@
+package api.poja.app.endpoint.event.model;
+
+import java.time.Duration;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString
+public class ImageSubmitted extends PojaEvent {
+  private UUID imageId;
+  private String fileName;
+  private String userEmail;
+  private String originalBucketKey;
+
+  @Override
+  public Duration maxConsumerDuration() {
+    return Duration.ofSeconds(60);
+  }
+
+  @Override
+  public Duration maxConsumerBackoffBetweenRetries() {
+    return Duration.ofSeconds(30);
+  }
+}
